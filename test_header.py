2baba389153a14b99a73ff29d0798813bd850ccf
YUCONG <205446753y@gmail.com>
2026-04-13 10:23:42 +0800

    Fix formatting with pre-commit

--- test_header.py
+++ test_header.py
@@ -5,20 +5,20 @@ Created on Sun Nov 23 00:19:30 2025
 @author: Severus
 """
 
-from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.patches import Circle
+from astropy.io import fits
 from astropy.wcs import WCS
 from matplotlib.colors import LogNorm
+from matplotlib.patches import Circle
 
-file_path0 = r'D:\spike_topping_type_III\2025\20250124\DEM\aia_data\aia.lev1_euv_12s.2025-01-24T044747Z.211.image_lev1.fits'
-file_path1 = r'D:\spike_topping_type_III\2025\20250124\share\Data\UnPack\20250124UT0447-0450\ImageData_RRLL\149MHz\RR\149MHz_2025124_043739_681.fits'
+file_path0 = r"D:\spike_topping_type_III\2025\20250124\RS_0447-0450\149MHz\149MHz_DeclinationDegree.fits"
+file_path1 = r"D:\spike_topping_type_III\2025\20250124\RS_0447-0450\149MHz\149MHz_RightAscensionDegree.fits"
 # 读取FITS文件
 hdul0 = fits.open(file_path0)
 header0 = hdul0[1].header
 hdul1 = fits.open(file_path1)
 header1 = hdul1[0].header
 print(header1)
-print('***********************')
+print("***********************")
 # print(header1)
